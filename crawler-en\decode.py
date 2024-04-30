73e2d4a248af913ede4f626377c95d269bc9133d
lain_e <103307000+Januarymyhat@users.noreply.github.com>
2024-04-30 17:21:26 +0800

    Update crawler-en\decode.py

--- crawler-en\decode.py
+++ crawler-en\decode.py
@@ -1,3 +1,5 @@
+# encode/decode
+
 name = 'dfa'
 print(name, type(name))
 
@@ -8,3 +10,18 @@ name2 = name1.decode()
 print(name2, type(name2))
 
 # 图片、视频、音频，需要以bytes类型的格式去保存
+
+
+'''
+requests基本使用
+'''
+import requests
+
+url = 'https://github.com/Januarymyhat'
+# 发送请求
+response = requests.get(url)
+print(response)
+
+# 打印响应内容
+print(response.text)  # 有乱码，requests模块会自动寻求一种解码方式去解码
+print(response.content.decode())  
